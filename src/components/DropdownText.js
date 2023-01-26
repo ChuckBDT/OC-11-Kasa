f217437aac8cfb602fdb4b0ede09944888131363
ChuckBDT <charlesboudrant@protonmail.com>
2023-01-26 05:48:25 +0100

    About Page added with accordions

---
+++ src/components/DropdownText.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Chevron from "../assets/Chevron";
+
+function DropdownText(props) {
+  const [opened, setOpened] = useState(false);
+
+  const toggle = () => {
+    if (opened) {
+      setOpened(false);
+    } else {
+      setOpened(true);
+    }
+  };
+
+  return (
+    <div className="accordion w-2/3">
+      <div className="item ">
+        <div
+          className="title bg-color-primary text-white flex justify-between items-center rounded font-medium h-12 px-4 cursor-pointer"
+          onClick={toggle}
+        >
+          <h2>{props.title}</h2>
+          <Chevron
+            color="white"
+            opened={
+              opened
+                ? "chevron rotate-180 transition-transform"
+                : "chevron rotate-0 transition-transform"
+            }
+          />
+        </div>
+        <p
+          className={
+            opened
+              ? "content bg-color-secondary txt-color-primary rounded-b leading-8 font-normal px-7 about-text show"
+              : "content bg-color-secondary txt-color-primary rounded-b leading-8 font-normal px-7 about-text"
+          }
+        >
+          {props.text}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default DropdownText;
--- src/views/APropos.js
+++ src/views/APropos.js
@@ -1,5 +1,50 @@
 import React from "react";
+import AboutHero from "../assets/pictures/about/about_hero.webp";
+
+import DropdownText from "../components/DropdownText";
+import { useState } from "react";
 
 export default function APropos() {
-  return <div>APropos</div>;
+  // ToDo: IMG -> PICTURE
+  // IMG BG BLEND ?
+  // Padding inside texts
+  // Content stretch
+
+  const [items, setItems] = useState([
+    {
+      id: 0,
+      title: "Fiabilité",
+      text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      id: 1,
+      title: "Respect",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      id: 2,
+      title: "Service",
+      text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      id: 3,
+      title: "Sécurité",
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]);
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-8 text-2xl mb-8">
+      <img
+        className="h-56 w-full object-cover rounded-3xl"
+        src={AboutHero}
+        alt=""
+      />
+      {items.map((item) => {
+        return (
+          <DropdownText key={item.id} title={item.title} text={item.text} />
+        );
+      })}
+    </div>
+  );
 }
